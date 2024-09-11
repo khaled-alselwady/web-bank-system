f9db6a88223fd825cc736ebdca76a789e05fc5d2
khaled-alselwady <Khalidsliman2014@gmail.com>
2024-09-11 04:55:45 +0300

    feat(frontend:clients): add table and filter components to clients component

--- BankSystem.Frontend/src/app/app-routing.module.ts
+++ BankSystem.Frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainStartupComponent } from './components/main-startup/main-startup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
 
 const appRoutes: Routes = [
   { path: '', component: MainStartupComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'clients', component: ClientsComponent },
   { path: '**', component: MainStartupComponent },
 ];
 
