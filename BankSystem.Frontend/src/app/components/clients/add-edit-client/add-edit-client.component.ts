9824e9a9123e5d81c18710f8dcfde00c9bcf2faf
khaled-alselwady <Khalidsliman2014@gmail.com>
2024-09-15 03:42:49 +0300

    feat(frontend:add-clients): add functionality to create client and person

--- BankSystem.Frontend/src/app/components/clients/add-edit-client/add-edit-client.component.ts
+++ BankSystem.Frontend/src/app/components/clients/add-edit-client/add-edit-client.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AddEditClient } from 'src/app/models/client/add-edit-client.model';
 import { ClientsDataService } from 'src/app/services/clients-data.service';
+import { ClientsService } from 'src/app/services/clients.service';
 import { FormService } from 'src/app/services/form.service';
 
 @Component({
@@ -18,6 +20,7 @@ export class AddEditClientComponent implements OnInit {
     private fb: FormBuilder,
     private formService: FormService,
     private clientsDataService: ClientsDataService,
+    private clientsService: ClientsService,
     private router: Router,
     private activatedRoute: ActivatedRoute
   ) {}
@@ -27,16 +30,21 @@ export class AddEditClientComponent implements OnInit {
       accountNumber: ['', Validators.required],
       pinCode: ['', [Validators.required, Validators.minLength(4)]],
       balance: ['', [Validators.required, Validators.min(0)]],
-      isActive: [true], // Default value set to 'true'
+      isActive: [true],
     });
   }
 
   // You can create a method to handle form submission
   onSubmit(): void {
-    if (this.clientInfoForm.valid) {
-      console.log(this.clientInfoForm.value);
-      console.log(this.personInfo);
-      // Perform further actions such as API calls, etc.
+    if (this.clientInfoForm.valid && this.isPersonFormValid) {
+      const clientData: AddEditClient = {
+        accountNumber: this.clientInfoForm.value.accountNumber,
+        pinCode: this.clientInfoForm.value.pinCode,
+        balance: this.clientInfoForm.value.balance,
+        isActive: this.clientInfoForm.value.isActive,
+        person: this.personInfo,
+      };
+      this.clientsService.add(clientData).subscribe((newClient) => {});
     } else {
       this.clientInfoForm.markAllAsTouched(); // This will trigger validation messages
     }
