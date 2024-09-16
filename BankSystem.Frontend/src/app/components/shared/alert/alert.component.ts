7cc2896b02cfb4fa98b69787d6e1c99635b26200
khaled-alselwady <Khalidsliman2014@gmail.com>
2024-09-16 04:16:46 +0300

    feat(frontend:shared): add template and styling for AlertComponent

--- BankSystem.Frontend/src/app/components/shared/alert/alert.component.ts
+++ BankSystem.Frontend/src/app/components/shared/alert/alert.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss']
+  styleUrls: ['./alert.component.scss'],
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent {
+  @Input() message: string = '';
+  @Input() type: 'success' | 'error' = 'success';
+  isVisible: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  show() {
+    this.isVisible = true;
+    setTimeout(() => (this.isVisible = false), 3000); // Auto-hide after 3 seconds
   }
-
 }
